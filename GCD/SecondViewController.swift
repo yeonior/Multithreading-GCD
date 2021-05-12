58b1d270abad3e1d6f65ae406ad4e4f6134fce87
Rus <de.mr44@gmail.com>
2021-05-12 14:22:55 +0500

    Added image uploading

--- GCD/SecondViewController.swift
+++ GCD/SecondViewController.swift
@@ -8,9 +8,34 @@
 import UIKit
 
 class SecondViewController: UIViewController {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    
+    fileprivate var imageURL: URL?
+    fileprivate var image: UIImage? {
+        get {
+            return imageView.image
+        }
+        
+        set {
+            activityIndicator.stopAnimating()
+            activityIndicator.isHidden = true
+            imageView.image = newValue
+            imageView.sizeToFit()
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        fetchImage()
+    }
+    
+    fileprivate func fetchImage() {
+        imageURL = URL(string: "https://images.unsplash.com/photo-1620800390262-00f3ff478212?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=3300&q=80")
+        activityIndicator.isHidden = false
+        activityIndicator.startAnimating()
+        guard let url = imageURL, let imageData = try? Data(contentsOf: url) else { return }
+        self.image = UIImage(data: imageData)
     }
 }
