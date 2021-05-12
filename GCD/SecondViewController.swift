04dea33a2138a4d3ecd0a793f68fac131395f7e8
Rus <de.mr44@gmail.com>
2021-05-12 13:15:03 +0500

    First commit

---
+++ GCD/SecondViewController.swift
@@ -0,0 +1,16 @@
+//
+//  SecondViewController.swift
+//  GCD
+//
+//  Created by ruslan on 11.05.2021.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
---
+++ GCD/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  GCD
+//
+//  Created by ruslan on 11.05.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
+
